c881d8e7dc8df865ef3a3ff084588ec9618505af
foooooooooooooooooooooooooootw <74590840+foooooooooooooooooooooooooootw@users.noreply.github.com>
2024-07-15 03:01:13 +0800

    Update dd.js
    fixed a bug where it only removes the badge from the first person and none of the others. 
    
    also made it so it executes different code on different parts of the website.

--- dd.js
+++ dd.js
@@ -1,62 +1,32 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-window.onload = async function() {
-    await delay(1000)
-
-    console.log(document.body.getAttribute("data-theme"))
-    document.body.setAttribute("data-theme", "purple")
-
-    const divToRemove = document.querySelector('.masonry__item.masonry__item--span70.card.animate--appear');
-    const divToRemove2 = document.querySelector('.badge.badge--blm.ng-star-inserted');
-    
-    if (divToRemove) {
-        // Remove the div from the DOM
-        divToRemove.parentNode.removeChild(divToRemove);
-      } else {
-        console.log('Div not found');
-      }
-
-
-    if (divToRemove2) {
-        // Remove the div from the DOM
-        divToRemove2.parentNode.removeChild(divToRemove2);
-      } else {
-        console.log('Div not found');
-      }
-
-    console.log('executed')
-};
-
 window.navigation.addEventListener("navigate", async (event) => {
 
   console.log('location changed!')
   
+  //race condition, will change soon
   await delay(1000)
 
   console.log(document.body.getAttribute("data-theme"))
+  //The default theme's name has changed to "default" but purple works for some reason, so it'll stay as it is in case they decide to change the default to something else.
   document.body.setAttribute("data-theme", "purple")
 
   const divToRemove = document.querySelector('.masonry__item.masonry__item--span70.card.animate--appear');
-  const divToRemove2 = document.querySelector('.badge.badge--blm.ng-star-inserted');
+  let divToRemove2 = document.querySelectorAll('.badge.badge--blm.ng-star-inserted');
   
-  if (divToRemove) {
-      // Remove the div from the DOM
+  if (window.location.href === "https://trials.report/") {
+    if (divToRemove) {
       divToRemove.parentNode.removeChild(divToRemove);
-    } else {
-      await delay(5000)
-      divToRemove.parentNode.removeChild(divToRemove);
-      console.log('Div not found');
-    }
-
+    } 
+ } else{
 
   if (divToRemove2) {
-      // Remove the div from the DOM
-      divToRemove2.parentNode.removeChild(divToRemove2);
-    } else {
-      await delay(5000)
-      divToRemove.parentNode.removeChild(divToRemove);
-      console.log('Div not found');
-    }
+      divToRemove2.forEach(function(element) {
+        element.parentNode.removeChild(element);
+    });
 
   console.log('executed')
-})
+ }
+}
+}
+)
